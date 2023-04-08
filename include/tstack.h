e55a3640a7299b726b78a7cbadbd298fe097b500
Dimakh1 <63556193+Dimakh1@users.noreply.github.com>
2023-04-08 22:03:55 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,26 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T * node;
+    int head;
+ public:
+    TStack() {
+        node = new T[size];
+        head = -1;
+    }
+    bool isEmpty() {
+        return head == -1;
+    }
+    void push(T a) {
+        node[++head] = a;
+    }
+    T peek() {
+        return node[head];
+    }
+    T pop() {
+        return node[head--];
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
